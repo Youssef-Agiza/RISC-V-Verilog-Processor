1e9b60bd377ee28f214b86a2d4045cc08fe9420b
Youssef Agiza <youssefagiza@aucegypt.edu>
2021-11-21 12:18:37 +0200

    removes unnecessary testing folders and adds branch test case

--- tests/hexToBinBytesOfsset.cpp
+++ tests/hexToBinBytesOfsset.cpp
@@ -9,17 +9,17 @@ std::string toStr(int n)
 }
 int main(int argc, char *argv[])
 {
-    // if (argc < 3)
-    // {
-    //     std::cout << "Invalid number of arguments\n";
-    //     exit(1);
-    // }
+    if (argc < 3)
+    {
+        std::cout << "Invalid number of arguments\n";
+        exit(1);
+    }
     std::string input_file_name = argv[1];  //assign input file path here
     std::string output_file_name = argv[2]; //assign output file path here
 
     std::ifstream ins(input_file_name);
     std::ofstream outs(output_file_name);
-    if (!ins.is_open() || !outs.is_open())
+    if (!ins.is_open())
     {
         std::cout << "Couldn't Open the file for reading\n";
         exit(1);
@@ -28,13 +28,13 @@ int main(int argc, char *argv[])
     // freopen(output_file_name, "w", stdout);
     int count = 0;
     std::string inputStr;
-    inputStr = "{mem[" + toStr(count + 3) + "], mem[" + toStr(count + 2) + "], mem[" + toStr(count + 1) + "], mem[" + toStr(count) + "]} = 32'h13;";
+    inputStr = "{mem[" + toStr(count + 3) + "+offset], mem[" + toStr(count + 2) + "+offset], mem[" + toStr(count + 1) + "+offset], mem[" + toStr(count) + "+offset]} = 32'h13;";
     outs << inputStr << std::endl;
     count += 4;
     while (!ins.eof())
     {
         std::getline(ins, inputStr);
-        inputStr = "{mem[" + toStr(count + 3) + "], mem[" + toStr(count + 2) + "], mem[" + toStr(count + 1) + "], mem[" + toStr(count) + "]} = 32'h" + inputStr + ";";
+        inputStr = "{mem[" + toStr(count + 3) + "+offset], mem[" + toStr(count + 2) + "+offset], mem[" + toStr(count + 1) + "+offset], mem[" + toStr(count) + "+offset]} = 32'h" + inputStr + ";";
         outs << inputStr << std::endl;
         count += 4;
     }
